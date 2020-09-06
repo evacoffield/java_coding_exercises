0f483e5554cfbe5bb496f4dc938286104a48bbd6
Eva Coffield <eva@darcoff.com>
2020-09-06 17:25:11 +0100

    Implementd exercises 002 - checkIsFromManchester

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,8 +2,8 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        return false;
+    public boolean isFromManchester(Person person) {
+        return "Manchester".equals(person.getCity());
     }
 
     public boolean canWatchFilm(/* Person person, int ageLimit*/) {
--- src/test/java/com/techreturners/exercise001/Exercise001Test.java
+++ src/test/java/com/techreturners/exercise001/Exercise001Test.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class Exercise001Test {
